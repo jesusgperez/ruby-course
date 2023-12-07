6d4fabb68d8fafc5834864c4c7093318e3ea126d
Jesus Perez <jesus.perez@uplight.com>
2023-12-07 15:33:24 -0500

    SOme comparison operators

---
+++ comparison.rb
@@ -0,0 +1,18 @@
+puts 10 > 5
+puts true
+puts 10 > 10
+puts 10 >= 10
+puts 10 <=> 10
+puts 20 <=> 10
+puts 10 <=> 20
+puts "hello" == "hello"
+#Just compares value
+puts 1 == 1.0
+#Compares value and type
+puts 1.eql?(1.0)
+#Compares if it's the same value
+puts "hola".eql?("hola")
+#Compares if it's the same value in memory
+puts "hola".equal?("hola")
+#Difference
+puts 10 != 20
