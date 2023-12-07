d60b92ce859abfbb3d6c2232fa399ba27f0f7a94
Jesus Perez <jesus.perez@uplight.com>
2023-12-07 15:38:27 -0500

    Look at some logical operators

---
+++ logical_operators.rb
@@ -0,0 +1,10 @@
+# And
+puts (10 > 5) && (5 < 8) # recommended
+puts (10 > 5) and (5 < 8)
+# Or
+puts (10 > 5) || (5 > 8) # recommended
+puts (10 > 5) or (5 > 8)
+
+#not
+puts !false
+puts (not false)
