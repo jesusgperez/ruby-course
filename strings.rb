98264b3d20961c1ac58d349d7e22d94f68b7f84d
Jesus Perez <jesus.perez@uplight.com>
2023-12-07 14:26:19 -0500

    Looking at some sring manipulation

---
+++ strings.rb
@@ -0,0 +1,14 @@
+nombre = "jesus"
+
+
+#These are the same
+puts "Hola " + nombre
+puts "Hola #{nombre}"
+#Operations permited as well
+puts "Hola #{20+20}"
+
+#Prints in console all the methods for a given object
+#puts "".methods
+
+puts nombre.upcase
+puts nombre.downcase
--- variables.rb
+++ variables.rb
@@ -2,3 +2,14 @@ name = 'jesus'
 age = 28
 
 puts 'Your name is %s and your age is %s' % [name, age]
+
+#Numbers are objects instead of primitives, so they have methods
+number = 12.to_f
+puts number
+
+#Division is always integer division
+puts 10/3
+
+#Float must be explicitly specified
+puts 10.0/3.0
+puts 10.to_f/3.to_f
