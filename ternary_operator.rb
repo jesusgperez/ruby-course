86a02f4d89b70d5a1973b8865f78789aadd5c5db
Jesus Perez <jesusgperez16@gmail.com>
2023-12-08 16:52:19 -0500

    Study about the ternary operator

---
+++ ternary_operator.rb
@@ -0,0 +1,18 @@
+print 'Escribe un nombre: '
+user = gets.chomp
+
+if user == 'jesus'
+	puts 'Tutor'
+else
+	puts 'Estudiante'
+end
+
+puts (if user == 'jesus' then 'tutor' else 'visitante' end)
+
+response = if user == 'jesus' then 
+	'tutor'
+else
+	'visitante'
+end
+
+puts response
