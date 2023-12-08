f84c219aae9d8f30526f2f678c6b3ee4ebe811de
Jesus Perez <jesusgperez16@gmail.com>
2023-12-08 17:36:58 -0500

    Giving a glance at arrays

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,17 @@
-array = []
+# One empty array
+array = [3,4,5]
 
+# Five empty spaces
+array2 = Array.new(5)
 
+puts array
+puts array[0]
+puts array2
+
+# Initializes the array with spaces
+array3 = %w[1 40 'strings]
+puts array3
+
+# Insert elements to the array
+array3 << 'hola'
+puts array3
