790281663ce79c982749693aa5c8c0c026d023f4
Jesus Perez <jesusgperez16@gmail.com>
2023-12-09 17:54:14 -0500

    About ranges

---
+++ ranges.rb
@@ -0,0 +1,29 @@
+numbers = (1..10)
+
+numbers.each do |number|
+	print number
+end
+
+puts
+
+(1..20).step(2).each do |number|
+	print number
+end
+
+puts
+
+('a'..'z').each do |letter|
+	print letter
+end
+
+puts
+
+(1..20).to_a.reverse.each do |number|
+	print number
+end
+
+puts
+
+print ('ma'..'md').to_a
+
+puts
