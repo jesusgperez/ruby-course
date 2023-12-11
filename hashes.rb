83bf694bfce55ac56b9fd1929bce1c1b09af4c8a
Jesus Perez <jesus.perez@uplight.com>
2023-12-11 18:13:13 -0500

    More about hashes

--- hashes.rb
+++ hashes.rb
@@ -14,3 +14,25 @@ puts hash['food']
 hash.each do |key, value|
   puts key, value
 end
+
+
+puts hash.length
+
+puts hash.has_key?('name')
+
+print hash.keys
+print hash.values
+
+puts
+
+# Another hash with just symbols
+
+hash2 = {name: 'ruth', last: 'ramirez', age: 24}
+
+puts hash.has_key?('age')
+
+hash2.delete(:last)
+
+puts hash2
+
+puts hash2.invert
