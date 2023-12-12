d10817ba484293d2ae4cd7836ccf60bf22dd8b9b
Jesus Perez <jesus.perez@uplight.com>
2023-12-12 16:59:02 -0500

    Something about polimorphism

---
+++ polimorphism.rb
@@ -0,0 +1,26 @@
+# There are no interfaces in Ruby
+
+class Video
+    def play
+        p "Generic video"
+    end
+end
+
+class Vimeo < Video
+    def play
+        p "Inserta el reproductor de vimeo"
+    end
+end
+
+class YouTube < Video
+    def play
+        p "Inserta el reproductor de YouTube"
+    end
+end
+
+videos = [Vimeo.new, YouTube.new, Video.new, Vimeo.new, Video.new, YouTube.new]
+
+
+videos.each do |video|
+    video.play
+end
